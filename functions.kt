9cd511ff660323af9ddd5aeb448ad86cb2da7749
Kirill Lemesev= <boyanya@gmail.com>
2023-03-29 22:42:10 +0500

    fun add fix

--- functions.kt
+++ functions.kt
@@ -5,26 +5,36 @@ fun add(){
 
     println("The card")
     var term = readln()
-    while (term in termDefinition.keys) {
+    while (termDefinition.containsKey(term)) {
         println("The card \"$term\" already exists. Try again:")
         term = readln()
     }
     println("The definition of the card:")
     var definition = readln()
-    while (definition in termDefinition.values) {
+    while (termDefinition.containsValue(definition)) {
         println("The card \"$term\" already exists. Try again:")
         definition = readln()
     }
 
-
+    termDefinition[term] = definition
     println("The pair (\"$term\":\"$definition\") has been added.")
 }
 
-/*
-fun remove(){
+
+/*fun remove(){
+    println("Which card?")
+    val cardName = readln()
+    if (termDefinition.containsKey(cardName)) {
+        termDefinition.remove(cardName)
+        println("The card has been removed.")
+    } else {
+        println("Can't remove \"$cardName\": there is no such card")
+    }
 
 }
 
+
+/*
 
 fun import(){
 
