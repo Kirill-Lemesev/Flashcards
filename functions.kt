2cafb028feecc06efce4dd5e00fab432f5ff8b9f
Kirill Lemesev= <boyanya@gmail.com>
2023-04-11 00:21:13 +0500

    fixes and improvments

--- functions.kt
+++ functions.kt
@@ -1,25 +1,26 @@
 package flashcards
 
 import java.io.File
-import kotlin.random.Random
+
 
 fun add() {
 
-    println("The card")
-    var term = readln()
-    while (termDefinition.containsKey(term)) {
-        println("The card \"$term\" already exists. Try again:")
-        term = readln()
-    }
-    println("The definition of the card:")
-    var definition = readln()
-    while (termDefinition.containsValue(definition)) {
-        println("The card \"$term\" already exists. Try again:")
-        definition = readln()
+    println("The card:")
+    val term = readln()
+    if (termDefinition.containsKey(term)) {
+        println("The card \"$term\" already exists.")
+    } else {
+        println("The definition of the card:")
+        val definition = readln()
+        if (termDefinition.containsValue(definition)) {
+            println("The definition \"$definition\" already exists.")
+        } else {
+            termDefinition[term] = definition
+            println("The pair (\"$term\":\"$definition\") has been added.")
+        }
+
     }
 
-    termDefinition[term] = definition
-    println("The pair (\"$term\":\"$definition\") has been added.")
 }
 
 
@@ -30,7 +31,6 @@ fun remove() {
     if (termDefinition.containsKey(cardName)) {
         termDefinition.remove(cardName)
         println("The card has been removed.")
-        println(termDefinition)
     } else {
         println("Can't remove \"$cardName\": there is no such card")
     }
@@ -39,56 +39,57 @@ fun remove() {
 
 
 fun import() {
+
     println("File name:")
     val file = File(readln())
-    var counter = 0
-
     if (file.exists()) {
+        var counter = 0
         file.forEachLine {
+            val keyValue = it.split(":")
+            termDefinition[keyValue[0]] = keyValue[1]
             counter += 1
-            val keyValue = it.split(" ")
-            if (keyValue[0] in termDefinition.values) {
-                termDefinition[keyValue[0]] = keyValue[1]
-            } else {
-                termDefinition[keyValue[0]] = keyValue[1]
-            }
         }
         println("$counter cards have been loaded")
+
     } else {
-        println("File not found")
-    }
+            println("File not found")
+        }
 }
 
 
 fun export() {
+
     println("File name:")
     val file = File(readln())
-    file.writeText(termDefinition.toString())
-    println("${termDefinition.size} cards have been saved")
+    var counter = 0
+    file.writeText("")
+    for ((key, value) in termDefinition) {
+        file.appendText("$key:$value\n")
+        counter += 1
+    }
+    //file.writeText(termDefinition.toString())
+    println("$counter cards have been saved")
 }
 
 
 fun ask() {
+
     println("How many times to ask?")
     val numberOfCards = readln().toInt()
     repeat(numberOfCards) {
         val randomKey = termDefinition.keys.random()
-        println("The definition for \"${termDefinition[randomKey]}\"")
-        val answer = readln()
-        when (answer) {
+        println("Print the definition of \"$randomKey\":")
+        when (val answer = readln()) {
             termDefinition[randomKey] -> {
                 println("Correct!")
             }
-
             in termDefinition.values -> {
                 for (key in termDefinition.keys) {
                     if (answer == termDefinition[key]) {
-                        println("Wrong. The right answer is \"${termDefinition[randomKey]}\", but your definition is correct for \"${termDefinition[key]}\".")
+                        println("Wrong. The right answer is \"${termDefinition[randomKey]}\", but your definition is correct for \"$key\"")
                     }
-
                 }
             }
-
             else -> {
                 println("Wrong. The right answer is \"${termDefinition[randomKey]}\"")
             }
